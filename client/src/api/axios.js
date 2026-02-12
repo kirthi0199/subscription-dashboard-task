7969ca6b2feb36457d30ce4dda203eec5aa3f4aa
kirthizuppa <kiruthikazuppa@gmail.com>
2026-02-12 10:39:34 +0530

    dep

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",   // <-- CRITICAL
+  baseURL: "http://subscription-dashboard-task-gk6i.vercel.app/api",   // <-- CRITICAL
 });
 
 api.interceptors.request.use(config => {
